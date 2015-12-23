aa970d434bf4e236163cd5a037cb87a0d00770fe
Roberto Orgiu <roberto.orgiu@gmail.com>
2015-12-23 13:27:08 +0100

    Adds test app

---
+++ app/src/main/java/it/ennova/rxwifi/testapp/TestActivity.java
@@ -0,0 +1,31 @@
+package it.ennova.rxwifi.testapp;
+
+import android.net.wifi.ScanResult;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import it.ennova.rxwifi.RxWifi;
+import rx.functions.Action1;
+
+public class TestActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        RxWifi.from(this).subscribe(new Action1<ScanResult>() {
+            @Override
+            public void call(ScanResult result) {
+                Log.d("RX-WIFI-Reactive", result.SSID + " { " + result.capabilities + " } " + result.frequency);
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                Log.d("RX-WIFI-Reactive", throwable.getMessage());
+            }
+        });
+    }
+
+}
