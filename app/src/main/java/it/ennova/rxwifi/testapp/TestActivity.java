875169efbb1e90ff8b5291cde9fc497f4feb31f4
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-06-06 15:25:18 +0200

    Refactors the TestActivity

--- app/src/main/java/it/ennova/rxwifi/testapp/TestActivity.java
+++ app/src/main/java/it/ennova/rxwifi/testapp/TestActivity.java
@@ -9,9 +9,13 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.List;
+
 import it.ennova.rxwifi.RxWifi;
 import it.ennova.rxwifi.internals.ScanResultUtils;
+import rx.functions.Action0;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class TestActivity extends AppCompatActivity{
     private final int PERMISSION_CODE = 1;
@@ -41,12 +45,34 @@ public class TestActivity extends AppCompatActivity{
         RxWifi.from(getApplicationContext()).subscribe(new Action1<ScanResult>() {
             @Override
             public void call(ScanResult result) {
-                Log.d("RX-WIFI-Reactive", ScanResultUtils.toWiFiNetwork(result).toString());
+                Log.d("RX-WIFI-SingleScan", ScanResultUtils.toWiFiNetwork(result).toString());
             }
         }, new Action1<Throwable>() {
             @Override
             public void call(Throwable throwable) {
-                Log.d("RX-WIFI-Reactive", throwable.getMessage());
+                Log.e("RX-WIFI-SingleScan", throwable.getMessage());
+            }
+        });
+
+        RxWifi.from(getApplicationContext(), 5).map(new Func1<List<ScanResult>, Integer>() {
+            @Override
+            public Integer call(List<ScanResult> results) {
+                return results.size();
+            }
+        }).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                Log.d("RX-WIFI-MultipleScans", "Found " + integer + " networks");
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                Log.e("RX-WIFI-MultipleScans", throwable.getMessage());
+            }
+        }, new Action0() {
+            @Override
+            public void call() {
+                Log.d("RX-WIFI-MultipleScans", "COMPLETED!");
             }
         });
     }
