4c692452a34737157346a13229bb53b442ce2b80
LorenzoEnnova <lorenzo.quiroli@gmail.com>
2017-02-17 12:40:09 +0100

    some more improvements

--- rxwifi/src/main/java/it/ennova/rxwifi/ResultReceiver.java
+++ rxwifi/src/main/java/it/ennova/rxwifi/ResultReceiver.java
@@ -6,9 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.support.annotation.NonNull;
 
 import rx.Observable;
+import rx.Subscription;
+import rx.functions.Action0;
 import rx.subjects.ReplaySubject;
 
 /**
@@ -23,10 +24,14 @@ class ResultReceiver extends BroadcastReceiver {
      */
     private ReplaySubject<ScanResult> subject;
     private final Context context;
+    private final WifiManager wifiManager;
+    private Subscription subscription;
 
+    @SuppressLint("WifiManagerPotentialLeak")
     public ResultReceiver(final Context context) {
         subject = ReplaySubject.create();
         this.context = context.getApplicationContext();
+        wifiManager = (WifiManager) this.context.getSystemService(Context.WIFI_SERVICE);
     }
 
     /**
@@ -34,21 +39,29 @@ class ResultReceiver extends BroadcastReceiver {
      */
     public ResultReceiver startScanningFrom() {
         context.registerReceiver(this, RxWifi.filter);
-        getWifiManager(context).startScan();
+        wifiManager.startScan();
         return this;
-    }
-
-    @SuppressLint("WifiManagerPotentialLeak")
-    private static WifiManager getWifiManager(@NonNull Context context) {
-        return (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(intent.getAction())) {
-            this.context.unregisterReceiver(this);
-            Observable.from((getWifiManager(this.context)).getScanResults())
+
+            subscription = Observable.from(wifiManager.getScanResults())
+                    .doAfterTerminate(new Action0() {
+                        @Override
+                        public void call() {
+                            unsubscribe();
+                        }
+                    })
                     .subscribe(subject);
+        }
+    }
+
+    private void unsubscribe() {
+        context.unregisterReceiver(this);
+        if (subscription != null && !subscription.isUnsubscribed()) {
+            subscription.unsubscribe();
         }
     }
 
